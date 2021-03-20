5d409ca2a47525a84f4cfd86de2674c8ac7b87dd
Pooja-1234567 <80240825+Pooja-1234567@users.noreply.github.com>
2021-03-20 19:46:12 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -74,18 +74,18 @@ resource "aws_vpc_peering_connection" "A2B" {
 #Instance on VPC A which is on private subnet.
 resource "aws_security_group" "default" {
      name        = "http-https-allow"
-     description = "Allow incoming HTTP and HTTPS and Connections"
+     description = "Allow SSH inbound traffic / Allow all outbound traffic"
      vpc_id      = "${aws_vpc.A.id}"
      ingress {
-         from_port = 80
-         to_port = 80
+         from_port = 22
+         to_port = 22
          protocol = "tcp"
          cidr_blocks = ["0.0.0.0/0"]
     }
      ingress {
-         from_port = 443
-         to_port = 443
-         protocol = "tcp"
+         from_port = 0
+         to_port = 0
+         protocol = "-1"
          cidr_blocks = ["0.0.0.0/0"]
     }
 resource "aws_instance" "dev_instance" {
@@ -106,18 +106,18 @@ resource "aws_instance" "dev_instance" {
 #Instance on VPC B on private subnet.
 resource "aws_security_group" "default" {
      name        = "http-https-allow"
-     description = "Allow incoming HTTP and HTTPS and Connections"
+     description = "Allow all traffic from local vpc"
      vpc_id      = "${aws_vpc.B.id}"
      ingress {
-         from_port = 80
-         to_port = 80
-         protocol = "tcp"
+         from_port = 0
+         to_port = 0
+         protocol = "-1"
          cidr_blocks = ["0.0.0.0/0"]
     }
      ingress {
-         from_port = 443
-         to_port = 443
-         protocol = "tcp"
+         from_port = 0
+         to_port = 0
+         protocol = "-1"
          cidr_blocks = ["0.0.0.0/0"]
     }
 resource "aws_instance" "dev_instance" {
