15b148048815e2fff5bc746b730331f20e0d7e24
Pooja-1234567 <80240825+Pooja-1234567@users.noreply.github.com>
2021-03-21 02:05:56 +0000

    Create eks_cluster.tf

---
+++ eks_cluster.tf
@@ -0,0 +1,32 @@
+#creating cluster on  existing vpc B
+
+data "aws_eks_cluster" "cluster" {
+  name = module.my-cluster.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.my-cluster.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "1.9"
+}
+
+module "my-cluster" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "my-cluster"
+  cluster_version = "1.17"
+  subnet         = ["${aws_subnet.public.id}"] 
+  vpc_id          = "${aws_vpc.B.id}"
+
+  worker_groups = [
+    {
+      instance_type = "m4.large"
+      asg_max_size  = 4
+    }
+  ]
+}
